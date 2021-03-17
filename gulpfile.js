a245f4779515664fe6381fd59675f438eb707c7e
Poll Castillo <alejandrocordovafree@gmail.com>
2021-03-17 00:31:46 -0500

    Añadido imagemin
    Ahora las imágenes perderán peso

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ const gulp        = require('gulp');
 const sass        = require('gulp-dart-sass');
 const pug         = require('gulp-pug-3');
 const browserSync = require('browser-sync').create();
+const imagemin    = require('gulp-imagemin');
+
+function minifyImg() {
+  gulp.src('default/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./images/'))
+}
 
 // Compile into CSS
 function compileSass() {
@@ -11,6 +18,7 @@ function compileSass() {
     .pipe(browserSync.stream());
 }
 
+// Compile into HTML
 function compilePug() {
   return gulp.src('./default/pug/*.pug')
     .pipe(pug({
@@ -20,6 +28,7 @@ function compilePug() {
     .pipe(browserSync.stream());
 }
 
+// Browser reload on change
 function watch() {
   browserSync.init({
     server: {
@@ -28,11 +37,13 @@ function watch() {
   });
 
   gulp.watch('./default/scss/**/*.scss', compileSass);
+  gulp.watch('./default/images/*', minifyImg);
   gulp.watch('./default/pug/*.pug', compilePug);
   gulp.watch('./*.html').on('change', browserSync.reload);
   gulp.watch('./js/**/*.js').on('change', browserSync.reload)
 }
 
 exports.compileSass = compileSass;
+exports.minifyImg = minifyImg;
 exports.compilePug = compilePug;
 exports.watch = watch;
