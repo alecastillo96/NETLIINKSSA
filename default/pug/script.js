83b9bff5fe755a0384067cd6e047d6359af636ea
Poll Castillo <acastillo@Polls-MacBook-Air.local>
2021-03-28 07:40:37 -0500

    El menú de navegación meovil está aquí

---
+++ default/pug/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+	let menuToggle = document.querySelector('.toggle');
+	let navigation = document.querySelector('.navigation');
+	menuToggle.classList.toggle('active')
+	navigation.classList.toggle('active')
+}
