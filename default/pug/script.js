01fcfbe37df76e0d1437eb5a00695346982befa8
Poll Castillo <acastillo@Polls-MacBook-Air.local>
2021-04-05 17:33:22 -0500

    Add js to tabs

--- default/pug/script.js
+++ default/pug/script.js
@@ -1,16 +1,29 @@
-function toggleMenu() {
+const toggleMenu = () => {
 	let menuToggle = document.querySelector('.toggle');
 	let navigation = document.querySelector('.navigation');
 	menuToggle.classList.toggle('active')
 	navigation.classList.toggle('active')
 }
 
+// Tabs
+document.querySelectorAll(".controls button").forEach(
+	button => {
+		button.addEventListener("click", (ev) => {
+			const parent = button.parentNode;
+			const granParent = parent.parentNode;
+			const container = granParent.querySelector(".tabs-container");
+			const childrenList = Array.from(parent.children);
+			const index = childrenList.indexOf(button);
+			container.style.transform = `translatex(-${index * 100}%)`;
+
+			parent.querySelectorAll("button.tabButtonActive").forEach (activeBtn => activeBtn.classList.remove("tabButtonActive"));
+			button.classList.add("tabButtonActive");
+		})
+	}
+)
+
 feather.replace();
 
+// tab-button_active
 
-function onclick() {
-	let subsSuccess  = document.querySelector('.success');
-	if (subsSuccess > 0) {
-		alert('Sies')
-	}
-}
+
